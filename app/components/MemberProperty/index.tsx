1c23dde05bedb35c21b507d07f3053631e1d5835
satooru65536 <ryuto4619@gmail.com>
2024-12-26 20:08:42 +0900

    change: MemberProperty のselectの表示値を変更

--- app/components/MemberProperty/index.tsx
+++ app/components/MemberProperty/index.tsx
@@ -1,6 +1,6 @@
 import { Input, Select, Text } from '@/components/basic';
 import dayjs from 'dayjs';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { styled } from 'restyle';
 
 interface StringProperty {
@@ -58,7 +58,17 @@ const IconImage = styled('img', {
 export default function MemberProperty({ editable = true, disabled = false, property, value, onChange, ...rest }: Props) {
   const [v, setV] = useState<typeof value>(value);
 
-  const valueString = rest.type !== 'date' ? value.toString() : dayjs(value).format(rest.format ?? 'YYYY年M月D日');
+  const valueString = useMemo(() => {
+    if (rest.type === 'date') {
+      return dayjs(value).format(rest.format ?? 'YYYY年M月D日');
+    }
+    else if (rest.type === 'select') {
+      return rest.options.find((option) => option.key === value)?.name ?? value;
+    }
+    else {
+      return value.toString();
+    }
+  }, [rest, value]);
 
   const showValueString = !(editable || (!editable && rest.type === 'icon'));
   const showInput = editable && rest.type !== 'select';
