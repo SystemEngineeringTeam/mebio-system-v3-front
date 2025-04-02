86eccfcb299ecd0d028c66a395cdff349047bd3c
hiroto09 <k23015kk@aitech.ac.jp>
2025-04-02 20:41:48 +0900

    init

--- app/pages/AdminPage/index.tsx
+++ app/pages/AdminPage/index.tsx
@@ -1,7 +1,23 @@
-export default function AdminPage() {
+import type { AdminLoaderData } from '@/routes/_main.admin';
+
+export default function AdminPage(members: AdminLoaderData) {
   return (
     <div>
-      <h1>管理者画面</h1>
+      <table className="mx-auto my-10 grid max-w-[600px] grid-cols-1 place-items-center gap-20 p-4 text-left sm:grid-cols-2">
+        <thead>
+          <tr>
+            <th className="text-lg font-bold">ユーザーID</th>
+            <th className="text-lg font-bold">アイコン</th>
+            <th className="text-lg font-bold">名前</th>
+            <th className="text-lg font-bold">フリガナ</th>
+            <th className="text-lg font-bold">学年</th>
+            <th className="text-lg font-bold">卒業年度</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
     </div>
   );
 }
--- app/routes/_main.admin.tsx
+++ app/routes/_main.admin.tsx
@@ -1,11 +1,16 @@
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 import AdminPage from '@/pages/AdminPage';
+import { MemberRepository } from '@/repository/member.repository';
+import { typedjson, useTypedLoaderData } from 'remix-typedjson';
 
-interface LoaderData {}
-
-export function loader(): LoaderData {
-  return { };
+export async function loader({ context }: LoaderFunctionArgs) {
+  const members = await MemberRepository.listWithSensitive(context.__prisma);
+  return typedjson({ members });
 }
 
+export type AdminLoaderData = Awaited<ReturnType<typeof loader>>;
+
 export default function Index() {
-  return <AdminPage />;
+  const { members } = useTypedLoaderData<typeof loader>();
+  return <AdminPage members={members}/>;
 };
