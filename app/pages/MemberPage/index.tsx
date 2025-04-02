76e9aed1af09f56c75cb56bd59ef1f5ae76e9071
hiroto09 <k23015kk@aitech.ac.jp>
2025-04-02 20:09:53 +0900

    update:一旦

--- app/pages/MemberPage/index.tsx
+++ app/pages/MemberPage/index.tsx
@@ -1,7 +1,38 @@
-export default function MemberPage() {
+import type { LoaderData } from '@/routes/_main.member.($uuid)';
+
+interface Props {
+  member: LoaderData;
+}
+export default function MemberPage({ member }: Props) {
   return (
-    <div>
-      <h1>メンバー画面</h1>
+    <div className="mx-auto my-10 grid max-w-[600px] grid-cols-1 place-items-center gap-20 p-4 text-left sm:grid-cols-2">
+      <h2 className="text-lg font-bold">ユーザーID</h2>
+      <p>{member.member.id}</p>
+
+      <h2 className="text-lg font-bold">アイコン</h2>
+      <img alt="Image" height={100} src={member.base.iconUrl} width={200} />
+
+      <h2 className="text-lg font-bold">名前</h2>
+      <p>{member.base.lastName + member.base.firstName}</p>
+
+      <h2 className="text-lg font-bold">フリガナ</h2>
+      <p>{member.base.lastNameKana + member.base.firstNameKana}</p>
+
+      {member.type === 'ACTIVE' && (
+        <>
+          <h2 className="text-lg font-bold">学年</h2>
+          <p>{member.detail.grade}</p>
+        </>
+      )}
+      {member.type === 'ALUMNI' && (
+        <>
+          <h2 className="text-lg font-bold">卒業年度</h2>
+
+          <div>
+            {member.detail.graduatedYear}
+          </div>
+        </>
+      )}
     </div>
   );
 }
