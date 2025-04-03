79cfad7b2c27820e5d40cef97480a522cc87f945
SatooRu65536 <ryuto65536@gmail.com>
2025-04-03 22:26:32 +0900

    feat: メッセージ

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -16,7 +16,8 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
 export default function LoginPage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center" data-scrollable="false">
-      <Form action="/auth/login" className="flex flex-col" method="post">
+      <Form action="/auth/login" className="text-center" method="post">
+        <p>ログインしてください</p>
         <Button type="submit">ログイン</Button>
       </Form>
     </div>
