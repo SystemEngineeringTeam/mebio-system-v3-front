103d949234dabf53f7a2a485bee3d59e0b0cc85b
SatooRu65536 <ryuto65536@gmail.com>
2025-04-04 00:31:09 +0900

    feat: UUIDとトークンを検証する新しいAPIローダーを追加

--- app/routes/api.token.$uuid.$token.tsx
+++ app/routes/api.token.$uuid.$token.tsx
@@ -1,11 +1,14 @@
 import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { MemberId } from '@/services/member.server';
 
-export async function loader({ context, params, request }: LoaderFunctionArgs) {
+export async function loader({ context, params }: LoaderFunctionArgs) {
   const { formService } = context.db.services;
 
   if (params.uuid === undefined) {
     throw new Response('UUID is required', { status: 400 });
+  }
+  if (params.token === undefined) {
+    throw new Response('Token is required', { status: 400 });
   }
 
   const memberId = MemberId.from(params.uuid).match(
@@ -13,16 +16,11 @@ export async function loader({ context, params, request }: LoaderFunctionArgs) {
     () => { throw new Response('Invalid UUID', { status: 400 }); },
   );
 
-  const url = new URL(request.url);
-  const token = url.searchParams.get('token');
-  if (token === null) {
-    throw new Response('Token is required', { status: 400 });
-  }
 
   // 検証
   const isValid = await formService.verifyToken(
     memberId,
-    token,
+    params.token,
   );
 
   return new Response(
